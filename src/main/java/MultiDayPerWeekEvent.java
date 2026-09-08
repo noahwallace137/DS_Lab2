85159e51c5d5c0a16b2592ac66194411d1356f43
Noah Wallace <noah.wallace@centre.edu>
2026-09-08 16:11:45 -0400

    Completed Lab 2

--- src/main/java/MultiDayPerWeekEvent.java
+++ src/main/java/MultiDayPerWeekEvent.java
@@ -1,13 +1,34 @@
+import java.util.GregorianCalendar;
+
 import calendar.MeetingCalendar;
 
 public class MultiDayPerWeekEvent extends CalendarEvent
 {
+
+	private int[] days;
+	public MultiDayPerWeekEvent(String description, String location, GregorianCalendar startTime,
+			GregorianCalendar endTime, GregorianCalendar repeatUntil, int[] days)
+	{
+		super(description, location, startTime, endTime);
+		this.days = days;
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
 	public void scheduleEvent(MeetingCalendar cal)
 	{
 		// TODO Auto-generated method stub
 
+	}
+
+	public int[] getDays()
+	{
+		return days;
+	}
+
+	public void setDays(int[] days)
+	{
+		this.days = days;
 	}
 
 }
